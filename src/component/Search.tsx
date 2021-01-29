30ffa586f17cfcb7552efb41a7096a054c2151cd
sergiutanasiev <tanasiev.sergiu@gmail.com>
2021-01-29 22:00:22 +0200

    debounce search query in fake api

--- src/component/Search.tsx
+++ src/component/Search.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import {SearchTerm} from './index'
+import _ from 'lodash'
+import * as data from '../api/company_collection.json'
+
 
 type Props = {}
 
@@ -8,6 +11,42 @@ export const Search:React.FC<Props> = ({}:Props) => {
 
     const onSearchTermChange = (searchTerm: string) => {
         setSearchTerm(searchTerm)
+        if (searchTerm.length > 3) {
+            delayQuery(searchTerm)
+        }
+    }
+
+    const delayQuery = _.debounce(searchTerm => getMatchingCompany(searchTerm), 750)
+
+    const getMatchingCompany = async(searchTerm: string) => {
+        await fetchDataPromise(searchTerm)
+        .then(response => {
+            console.log(response)
+        }).catch((error) => {
+            
+        })
+    }
+
+    const fetchDataPromise = (searchTerm: string):Promise<any> => {
+        // Make a local copy
+        let fakeApiData:any = Object.assign({}, Object.values(data)[0])
+        let results:Array<any> = []
+        // Run a promise and return macthed results
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                for (let i in fakeApiData) {
+                    if (fakeApiData[i].name.toLowerCase().includes(searchTerm)) {
+                        results.push(fakeApiData[i])
+                    }
+                }
+                if (results.length !== 0) {
+                    resolve(results)
+                } else {
+                    // Error message to output
+                    reject()
+                }
+            }, 500)
+        })
     }
 
     return(
